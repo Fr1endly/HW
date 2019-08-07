fd1b212579812a934cf993ae206b6ca5424ea772
Fr1endly <GizatulinsO@gmail.com>
2019-08-08 02:57:58 +0300

    router sends data from db to front page

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'loselose',
+  database : 'e_store'
+});
+
+connection.connect();
+
+var solution;
+connection.query('select * from user where ID;', function (error, results, fields) {
+  solution = results[0]
+  console.log(solution)
+});
+
+connection.end();
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var mysql      = require('mysql');
-  var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'loselose',
-    database : 'e_store'
-  });
-  
-  connection.connect();
-  
-  var solution;
-  connection.query('select * from user where ID;', function (error, results, fields) {
-    solution = results[0]
-    console.log(solution)
-  });
-  
-  connection.end();
+
   res.end(solution.first_name)
 });
 
